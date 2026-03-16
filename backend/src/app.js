d453dcc7328a0d452d470ea0373e5cf5134e1239
Chirag prajapat <kachholi0@gmail.com>
2026-03-16 10:44:28 +0530

    writing code for app.js

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+// Connect to MongoDB
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/services', require('./routes/serviceRoutes'));
+app.use('/api/bookings', require('./routes/bookingRoutes'));
+
+app.get('/', (req, res) => {
+  res.send('SkillBridge API is running...');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Server Error', error: err.message });
+});
+
+module.exports = app;
